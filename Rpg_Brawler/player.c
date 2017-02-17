f7c58afcbf14faa866f082a9134d99629656182a
StureTh <sturethuren@gmail.com>
2017-02-17 10:36:22 +0100

    asdf

---
+++ Rpg_Brawler/player.c
@@ -0,0 +1,38 @@
+#include "player.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+
+static player_t createPlayer(char *name)
+{
+    player_t new_player;
+    strcpy(new_player.name, name);
+    new_player.hp = 150;
+    new_player.coin_purse = 2000;
+
+    return new_player;
+}
+
+player_t PLAYER_initPlayer(void)
+{
+    player_t player;
+    char name[15];
+
+    printf("Enter player name: ");
+    scanf("%s", name);
+    player = createPlayer(name);
+
+    printf("Greetings traveler %s! "
+           "You have %d health and %d coins to buy what you need\n",
+
+           player.name,
+           player.hp,
+           player.coin_purse);
+
+    return player;
+}
+
+
+
+
